f80e6b804e11a06e1fc6122d4d7d1aacdfbdf689
ufdy <159640208+ufdy@users.noreply.github.com>
2024-02-10 19:25:07 +0200

    Add files via upload

---
+++ Res2/SecondLab/SecondLab.cpp
@@ -0,0 +1,22 @@
+﻿// Лабараторная работа #2 "Битовые операции"
+// 3 вариант
+
+
+#include <iostream>
+
+int main()
+{   
+    unsigned char x = 98; //0b01100010
+    //unsigned char x = 221; //0b11011101
+    //unsigned char x= 76; //0b01001100
+    //unsigned char x = 6; //0b00000110
+    short i;
+    std::cin >> i;
+    unsigned char mask = ~(0b00000001 << i);
+    unsigned char result = x & mask;
+    std::cout << int(x) << std::endl;
+    std::cout << int(result) << std::endl;
+}
+
+
+
